340711e5a9a57d0c5fe3ae84712e7327502a5904
mqasimca <one@Ubuntu.local>
2015-05-17 23:08:51 -0400

    First Commit

---
+++ ddos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+## Script To Check & Block DDOS ##
+## Written : 17 DEC 2014 ##
+## By Qasim
+
+netstat -plan|grep :80|awk {'print $5'}|cut -d: -f 1|sort|uniq -c|sort -nk 1 | tail -n 20 > /var/log/ddos/RTIPADR
+IPADR=`ifconfig | grep "inet addr" |awk '{print $2}' | grep -v 127 | cut -d : -f 2 > /var/log/ddos/IPADR`
+grep -Ff /var/log/ddos/IPADR -v /var/log/ddos/RTIPADR  > /var/log/ddos/Alpha
+for i in {1..20}; do
+	IPADR=`sed -n "$i"p /var/log/ddos/Alpha | awk '{print $2}'`
+	Connections=`sed -n "$i"p /var/log/ddos/Alpha | awk '{print $1}'`
+	if [[ $Connections -ge 1200 ]]; then
+		csf -d $IPADR
+	fi
+done
+
+awk '{print $1}' /usr/local/apache/logs/access_log | sort | uniq -c | sort -n | tail -n 200 > /var/log/ddos/ddosApacheAL.txt
+grep -Ff /var/log/ddos/IPADR -v /var/log/ddos/ddosApacheAL.txt  > /var/log/ddos/ddosApacheALEx.txt
+for dAal in {1..200}; do
+NOFL=`sed -n "$dAal"p /var/log/ddos/ddosApacheALEx.txt |  awk '{print $1}'`
+BIP=`sed -n "$dAal"p /var/log/ddos/ddosApacheALEx.txt |  awk '{print $2}'`
+if [[ $NOFL -ge 10000 ]]; then
+	csf -d $BIP
+fi
+done
+
